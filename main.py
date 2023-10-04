f9b959ece0539b23eb56af82cb96262197eacc4d
Master-Ess <67045069+Master-Ess@users.noreply.github.com>
2023-10-04 17:10:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API CONNECTED"
+    return "API CONNECTED - HI LEIA/LUKE/ROB"
 
 @app.route("/login/<email>/<hash>")
 def login(email, hash):
